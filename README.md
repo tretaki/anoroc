e0872597f83596e2ad4a5e84e01f842129436f75
tretaki <hikari.tretaki@gmail.com>
2020-03-23 10:25:28 +0100

    Add regions. Improve plotting.

--- README.md
+++ README.md
@@ -2,19 +2,30 @@
 
 Get the corona csv data from: https://github.com/CSSEGISandData/COVID-19/tree/master/csse_covid_19_data/csse_covid_19_time_series
 
-Plot the individual country confirmed cases data.
+Download and plot confirmed cases and deaths data.
 
 ## Usage
 
-python anoroc.py -c Italy
+Plot world data.
+`python anoroc.py`
+
+Plot one country data.
+`python anoroc.py -c Italy`
+
+Plot data for wider region. You can choose from: Africa, Asia, Europe, North America, Oceania, South America, World.
+`python anoroc.py -r Asia`
+
+You can exclude country from the data by using -e flag.
+`python anoroc.py -r World -e China`
+`python anoroc.py -r Europe -e Italy`
+
 
 If you want to re-download and update the data use -u flag.
-
-python anoroc.py -u -c Italy
+`python anoroc.py -u`
+`python anoroc.py -u -c Italy`
 
 ## TODOs
 
 + Add tests.
-+ Expand extract_region() function to be able to exclude regions.
-+ Add death and recovered data.
++ Add recovered data.
 + Improve plot functions to be able to plot multiple countries at same time.
--- anoroc.py
+++ anoroc.py
@@ -5,6 +5,7 @@ import pandas
 import numpy
 import matplotlib.pyplot as plt
 import argparse
+import yaml
 
 
 prog_descrip = """Pull csv corona data from github repo. Plot country cases."""
@@ -18,30 +19,59 @@ parser.add_argument(
     "-c", "--country", help="Input name of the country which you want to plot."
 )
 
+parser.add_argument(
+    "-r",
+    "--region",
+    help="Input name of the region which you want to plot. You can choose from Africa, Asia, Europe, North America, Oceania, South America.",
+)
+
+parser.add_argument(
+    "-e",
+    "--exclude",
+    help="Input name of the country which you want to be excluded from the region.",
+)
+
 args = parser.parse_args()
 
-url = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv"
+url_confirmed = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv"
 
-filename = "cases_data.csv"
+url_death = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Deaths.csv"
+
+url_recovered = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Recovered.csv"
+
+filename1 = "cases_confirmed.csv"
+filename2 = "cases_death.csv"
+filename3 = "cases_recovered.csv"
 
 # update the csv data if the update flag is passed
 if args.update:
-    if os.path.exists(filename):
-        os.remove(filename)
+    if os.path.exists(filename1):
+        os.remove(filename1)
+    if os.path.exists(filename2):
+        os.remove(filename2)
+    if os.path.exists(filename3):
+        os.remove(filename3)
 
 # download csv if file doesn't exist
-if not os.path.exists(filename):
-    filename = wget.download(url, out=filename)
+if not os.path.exists(filename1):
+    print("\n\nDownloading data.")
+    wget.download(url_confirmed, out=filename1)
+if not os.path.exists(filename2):
+    wget.download(url_death, out=filename2)
+if not os.path.exists(filename3):
+    wget.download(url_recovered, out=filename3)
 
 # load the data
-data = pandas.read_csv(filename)
+data_confirmed = pandas.read_csv(filename1)
+data_death = pandas.read_csv(filename2)
+data_recovered = pandas.read_csv(filename3)
 
-# get the column names from data frame
-cols = data.columns.to_numpy()
-dates = cols[4:]
+# Get the countries in continents from countries.yaml
+with open("countries.yaml", "r") as file:
+    countries = yaml.load(file, Loader=yaml.FullLoader)
 
 
-def extract_country(country, data, exclude=False):
+def extract_country(country, data):
     """ Extracts data for country.
 
     Parameters
@@ -50,8 +80,6 @@ def extract_country(country, data, exclude=False):
         Name of the country.
     data : numpy array
         All data loaded from the csv file.
-    exclude: boolean
-        If True it excludes this country from the data.
 
     Returns
     -------
@@ -60,22 +88,21 @@ def extract_country(country, data, exclude=False):
 
     """
 
-    if country == "all":
-        result = data
-    else:
-        if exclude:
-            result = data.loc[data["Country/Region"] != country]
-        else:
-            result = data.loc[data["Country/Region"] == country]
+    # extract data
+    count = data.loc[data["Country/Region"] == country]
 
-    result = result.to_numpy()
-    result = result[:, 4:]  # omitt the first 4 columns
-    result = result.sum(axis=0)  # add all data in subregions
+    count = count.to_numpy()
+    count = count[:, 4:]  # omitt the first 4 columns
+    count = count.sum(axis=0)  # add all data in subregions
 
-    return result
+    # extract dates
+    dates = data.columns.to_numpy()
+    dates = dates[4:]  # omitt the first 4 columns
+
+    return count, dates
 
 
-def extract_region(region, data):
+def extract_countries(countries, data):
     """ Extracts data for wider regions.
 
     Parameters
@@ -92,87 +119,142 @@ def extract_region(region, data):
 
     """
 
-    count = 0
-    for country in region:
-        count += extract_country(country, data)
+    counts = 0
+    for country in countries:
+        count, dates = extract_country(country, data)
+        counts += count
 
-    return count
+    return counts, dates
 
 
-def plot_country(country, exclude=False):
+def region(region_name, countries, data, exclude=[]):
+    """ Makes a list of countries in the region.
 
-    count = extract_country(country, data, exclude)
+    Parameters
+    ----------
+    region_name : string
+        Choose one of the continents (e.g. "Asia") or "World".
+    countries : dictionary
+        List of countries under certain continent, from countries.yaml.
+    data : numpy array
+        All data loaded from the csv file.
+    exclude: list
+        List of countries you want to exclude.
+
+    Returns
+    -------
+    region_name: string
+        Region name. If some countries are excluded that is written in the name.
+    region: list
+        Returns the countries in the region.
+    """
+
+    continents = list(countries.keys()) + ["World"]
+
+    if region_name not in continents:
+        print(
+            "Region name not valid. Choose from: \nAfrica, Asia, Europe, North America, Oceania, South America"
+        )
+        return None, None
+
+    region = []
+
+    # make the list of countries on certain continent or whole world
+    for country in data["Country/Region"]:
+        if region_name == "World":
+            region.append(country)
+        elif country in countries[region_name]:
+            region.append(country)
+
+    # deduplicate the list with multiple entries for the same country
+    region = list(set(region))
+
+    # remove excluded countries from the list
+    if type(exclude) is str:
+        exclude = [
+            exclude,
+        ]
+
+    excluded = []
+    for ex in exclude:
+        if ex in region:
+            excluded.append(ex)
+            region.remove(ex)
+
+    return region, excluded
+
+
+def plot_countries(countries, title=None):
+    """ Plot data with matplotlib.
+
+        Make four graphs: logscale cumulative cases, new cases,
+        cumulative deaths, new deaths
+    """
+
+    # count confirmed cases
+    count, dates = extract_countries(countries, data_confirmed)
     new_cases = count - numpy.insert(count, 0, 0)[:-1]
 
-    fig, ax1 = plt.subplots()
-    plt.xticks(rotation=90)
-    ax2 = ax1.twinx()
-    ax1.plot(dates, count, "ro-")
-    ax2.plot(dates, new_cases, "bo-")
-    ax1.set_yscale("log")
-    if exclude:
-        plt.title("Excluding " + country)
-    else:
-        plt.title(country)
+    # count death cases
+    count_d, dates_d = extract_countries(countries, data_death)
+    new_cases_d = count_d - numpy.insert(count_d, 0, 0)[:-1]
 
-    ax1.set_ylabel("Comulative Cases", color="r")
-    ax2.set_ylabel("New Cases", color="b")
+    # count recovered cases
+    count_r, dates_r = extract_countries(countries, data_recovered)
+    new_cases_r = count_r - numpy.insert(count_r, 0, 0)[:-1]
+
+    if count.any() == 0:
+        print("Data set empty. Probably you misspelled country name.")
+        return
+
+    fig, axs = plt.subplots(4, sharex=True)
+
+    axs[0].set_title(title)
+
+    plt.xticks(rotation=90)
+    label_c = "Confirmed: %s" % count[-1]
+    label_r = "Recovered: %s" % count_r[-1]
+    axs[0].plot(dates, count, "ro-", label=label_c)
+    axs[0].plot(dates, count_r, "go-", label=label_r)
+    axs[0].set_yscale("log")
+    axs[0].legend()
+    axs[1].bar(dates, new_cases)
+    axs[2].plot(dates_d, count_d, "ro-")
+    axs[3].bar(dates_d, new_cases_d)
+
+    axs[0].set_ylabel("Cumulative Cases\n Logscale", color="r")
+    axs[1].set_ylabel("New Cases", color="b")
+    axs[2].set_ylabel("Cumulative Deaths", color="r")
+    axs[2].annotate("Deaths: %s" % count_d[-1], xy=(1, count_d[-1] * 0.75))
+    axs[3].set_ylabel("New Deaths", color="b")
     plt.show()
 
-
-def plot_region(region, name=""):
-
-    count = extract_region(region, data)
-    new_cases = count - numpy.insert(count, 0, 0)[:-1]
-
-    fig, ax1 = plt.subplots()
-    plt.xticks(rotation=90)
-    ax2 = ax1.twinx()
-    ax1.plot(dates, count, "ro-")
-    ax2.plot(dates, new_cases, "bo-")
-    ax1.set_yscale("log")
-
-    plt.title(name)
-
-    ax1.set_ylabel("Comulative Cases", color="r")
-    ax2.set_ylabel("New Cases", color="b")
-    plt.show()
-
-
-EU = [
-    "Austria",
-    "Belgium",
-    "Bulgaria",
-    "Croatia",
-    "Cyprus",
-    "Czechia",
-    "Denmark",
-    "Estonia",
-    "Finland",
-    "France",
-    "Germany",
-    "Greece",
-    "Hungary",
-    "Ireland",
-    "Italy",
-    "Latvia",
-    "Lithuania",
-    "Luxembourg",
-    "Malta",
-    "Netherlands",
-    "Poland",
-    "Portugal",
-    "Romania",
-    "Slovakia",
-    "Slovenia",
-    "Spain",
-    "Sweden",
-]
 
 if __name__ == "__main__":
-    if args.country:
-        plot_country(str(args.country))
-    else:
-        print("No country selected. Plotting EU instead.")
-        plot_region(EU, name="EU")
 
+    if args.country:
+        country = args.country
+        plot_countries([str(country)], title=country)
+    elif args.region and args.exclude:
+        region_name = args.region
+        region, excluded = region(
+            region_name, countries, data_confirmed, exclude=[str(args.exclude)]
+        )
+        if region and excluded:
+            plot_countries(region, title=region_name + " excluding " + args.exclude)
+        elif region:
+            print(args.exclude + " not in " + args.region)
+            print("Plotting data for the whole region.")
+            plot_countries(region, title=region_name)
+    elif args.region:
+        region_name = args.region
+        region, _ = region(region_name, countries, data_confirmed)
+        if region:
+            plot_countries(region, title=region_name)
+    elif args.update:
+        print("\n\nFresh data downloaded.")
+    else:
+        print("\nNo country selected. Plotting data for whole world.")
+        world, _ = region("World", countries, data_confirmed)
+        if world:
+            plot_countries(world, title="World")
