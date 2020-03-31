a9049d4206af7992916de8a13beb65f8e5ab595e
tretaki <hikari.tretaki@gmail.com>
2020-03-31 22:19:02 +0200

    Fix data.py bug.

--- data.py
+++ data.py
@@ -8,6 +8,6 @@ def load(file_confirmed, file_death, file_recovered):
     # load the data
     confirmed = pandas.read_csv(file_confirmed)
     death = pandas.read_csv(file_death)
-    recovered = pandas.read_csv(file_death)
+    recovered = pandas.read_csv(file_recovered)
 
     return confirmed, death, recovered
